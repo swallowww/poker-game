b11820ce08ee9691e84f7bba69e23dced4bd7a90
swallowww <1105047416@qq.com>
2019-09-21 01:51:17 +0800

    增加翻牌；增加背景图案。

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,62 +1,90 @@
 <template>
-    <div class="poker-card" :style="'left:'+left+';top:'+top">
-        <div v-show="!card.cover">{{card.num}}{{card.color}}</div>
-        <div v-show="card.cover" class="poker-card-cover"></div>
-    </div>
+  <div
+    :class="{'poker-card':true,'cardCover':card.cover}"
+    :style="'left:'+left+';top:'+top+';background-position:'+findCardBg()"
+    v-on:click="cardClick"
+  ></div>
 </template>
 
 <script>
 export default {
-    name:'card',
-    props:{
-        card:{
-            type:Object,
-            default:{}
-        }
-    },
-    data(){
-        return {
-            left:'0.2rem',
-            top:'0.2rem',
-            topSide:2.5,//距离上方的rem数
-            leftSide:0.2,//距离左边的间隔rem数
-            cardWidth:1.2//扑克宽
-        }
-    },
-    mounted:function(){
-        if(this.card.position){
-            this.postionThisCard();
-        }
-    },methods:{
-        postionThisCard:function(){
-            this.left=((this.card.position.col-1)*(this.cardWidth+0.2)+this.leftSide)+'rem';
-            this.top=((this.card.position.rol-1)*0.5+this.topSide)+'rem';
-        }
+  name: "card",
+  props: {
+    card: {
+      type: Object,
+      default: {}
     }
-}
+  },
+  data() {
+    return {
+      left: "0.06rem",
+      top: "0.06rem",
+      topSide: 2.5, //距离上方的rem数
+      leftSide: 0.06, //距离左边的间隔rem数
+      cardWidth: 1.36 //扑克宽
+    };
+  },
+  mounted: function() {
+    if (this.card.position) {
+      this.postionThisCard();
+    }
+  },
+  methods: {
+    cardClick: function() {
+      if (this.card.cover && !this.card.position) {
+        this.card.cover = false;
+        this.$parent.addLook(this.card);
+      }
+    },
+    findCardBg: function() {
+      let res = "left top";
+      let bgWidth = -1.36;
+      let bgHeight = -2.06;
+      if (this.card.cover) {
+        let left = bgWidth * 2 + "rem";
+        let top = bgHeight * 4 + "rem";
+        return "left " + left + " top " + top;
+      }
+      {
+        let left = bgWidth * (this.card.index - 1) + "rem";
+        let top = bgHeight * this.card.color + "rem";
+        res = "left " + left + " top " + top;
+        return res;
+      }
+    },
+    postionThisCard: function() {
+      this.left =
+        (this.card.position.col - 1) * (this.cardWidth + this.leftSide) +
+        this.leftSide +
+        "rem";
+      this.top = (this.card.position.rol - 1) * 0.2 + this.topSide + "rem";
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-.poker-card{
-    width:1.2rem;
-    height: 2rem;
-    border-radius: 0.1rem;
-    float: left;
-    font-size: 0.3rem;
-    background-color:#fff;
-    // display: flex;
-    // flex: 1;
-    // justify-content: center;
-    // flex-direction: column;
-    // text-align: center;
-    position: relative;
-    overflow: hidden;
-    box-shadow: 0 0 0.02rem #999;
-    .poker-card-cover{
-        position: absolute;
-        width:100%;
-        height:100%;
-        background-color:#ccc;
-    }
+.poker-card {
+  width: 1.36rem;
+  height: 2.05rem;
+  border-radius: 0.1rem;
+  float: left;
+  font-size: 0.3rem;
+  background-color: #fff;
+  position: relative;
+  overflow: hidden;
+  user-select: none;
+  background-image: url(../assets/img/pokerBg.jpg);
+  background-repeat: no-repeat;
+  background-size: 17.7rem auto;
+  z-index: 100;
+  transition: left 0.3s;
+  transition-property: left, top;
+  .poker-card-cover {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: #ccc;
+  }
 }
 </style>
--- src/pages/game.vue
+++ src/pages/game.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="poker-game">
+    <ul class="poker-collect">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    <div class="poker-all" v-on:click="coverAgain"></div>
     <card
       class="cards"
       v-for="(card,index) in cards"
       :key="index"
-      :data-index="index"
+      :data-index="card.index+'-'+card.color"
+      :data-unique="card.unique"
       :card="card"
     />
   </div>
@@ -17,7 +25,10 @@ export default {
   name: "game",
   data() {
     return {
-      cards: []
+      cards: [],
+      look: [],
+      scrollZ: 100,
+      reversePokersId: []
     };
   },
   components: {
@@ -27,8 +38,53 @@ export default {
     this.initPoker();
   },
   methods: {
+    coverAgain: function() {
+      //重新翻牌
+      for (let i = 0; i < this.reversePokersId.length; i++) {
+        for (let j = 0; j < this.cards.length; j++) {
+          if (this.cards[j].unique == this.reversePokersId[i]) {
+            let element = document.querySelectorAll(
+              '[data-unique="' + this.cards[j].unique + '"]'
+            )[0];
+            element.style.zIndex = 100;
+            element.style.left = "0.06rem";
+            this.cards[j].cover = true;
+            break;
+          }
+        }
+      }
+      this.scrollZ = 100;
+      this.look.length = 0;
+    },
+    addLook: function(card) {
+      //翻牌
+      this.reversePokersId.push(card.unique);
+      if (this.look.length == 3) {
+        let backCard = this.look.shift();
+        let element = document.querySelectorAll(
+          '[data-index="' + backCard + '"]'
+        )[0];
+        this.scrollZ--;
+        element.style.zIndex = this.scrollZ;
+        let unique = element.getAttribute("data-unique");
+        for (let i = 0; i < this.cards.length; i++) {
+          if (this.cards[i].unique == unique) {
+            this.cards[i].cover = true;
+            break;
+          }
+        }
+      }
+      this.look.push(card.index + "-" + card.color);
+      for (let i = 0; i < this.look.length; i++) {
+        let element = document.querySelectorAll(
+          '[data-index="' + this.look[i] + '"]'
+        )[0];
+        element.style.left = 1.48 + i * 0.3 + "rem";
+        element.style.zIndex = i + 100;
+      }
+    },
     randArr: function(arr) {
-        //打乱顺序
+      //打乱顺序
       for (var i = 0; i < arr.length; i++) {
         var iRand = parseInt(arr.length * Math.random());
         var temp = arr[i];
@@ -38,6 +94,7 @@ export default {
       return arr;
     },
     initPosition: function(cards) {
+      //摆牌
       let card_index = 0;
       for (let i = 1; i <= 7; i++) {
         for (let j = 0; j < i; j++) {
@@ -45,33 +102,37 @@ export default {
             col: i,
             rol: j + 1
           };
-          cards[card_index].cover = false;
+          if (j < i - 1) {
+            cards[card_index].cover = true;
+          } else {
+            cards[card_index].cover = false;
+          }
           card_index++;
         }
       }
       return cards;
     },
-    openPoker: function() {
-      //开始摆扑克
-    },
     initPoker: function() {
       //生成扑克牌
       let newCards = [];
+      let unique = 0;
       for (let c = 0; c < 4; c++) {
         for (let i = 1; i <= 13; i++) {
           let obj = {
+            index: i,
             num: this.pokerNumber(i),
-            color: this.pokerColor(c),
-            cover: true
+            color: c,
+            cover: true,
+            unique: unique
           };
           newCards.push(obj);
+          unique++;
         }
       }
       //打乱
       newCards = this.randArr(newCards);
       //生成位置坐标
       this.cards = this.initPosition(newCards);
-      this.openPoker();
     },
     pokerNumber: function(i) {
       //生成扑克面值
@@ -79,40 +140,16 @@ export default {
         switch (i) {
           case 11:
             return "J";
-            break;
           case 12:
             return "Q";
-            break;
           case 13:
             return "K";
-            break;
           default:
             return "";
         }
       } else {
         return String(i);
       }
-    },
-    pokerColor: function(c) {
-      //生成扑克花色
-      let type = "";
-      switch (c) {
-        case 0:
-          type = "黑桃";
-          break;
-        case 1:
-          type = "红桃";
-          break;
-        case 2:
-          type = "梅花";
-          break;
-        case 3:
-          type = "方块";
-          break;
-        default:
-          type = "";
-      }
-      return type;
     }
   }
 };
@@ -131,6 +168,36 @@ export default {
     position: fixed;
     left: 0;
     top: 0;
+  }
+  .poker-all {
+    width: 1.36rem;
+    height: 2.05rem;
+    box-sizing: border-box;
+    border-radius: 0.1rem;
+    border: solid 0.03rem #000;
+    position: fixed;
+    left: 0.06rem;
+    top: 0.06rem;
+    opacity: 0.3;
+  }
+  .poker-collect {
+    width: 5.68rem;
+    position: fixed;
+    left: 4.32rem;
+    top: 0.06rem;
+    height: 2.1rem;
+    float: left;
+    li {
+      list-style: none;
+      width: 1.36rem;
+      height: 2.05rem;
+      box-sizing: border-box;
+      float: left;
+      border-radius: 0.1rem;
+      border: solid 0.03rem #000;
+      margin-right: 0.06rem;
+      opacity: 0.3;
+    }
   }
 }
 </style>
